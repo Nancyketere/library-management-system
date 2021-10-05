4af90281ef4f0d57512d5d10e703ee3855a23130
Nancyketere <91130030+Nancyketere@users.noreply.github.com>
2021-10-05 21:39:24 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+    Project: Library System
+    Author: Nancy Ketere
+    Date: 5-10-2021
+    Compiler: GCC
+    Licence: MIT
+*/
+
+#include<stdio.h>
+
+int main(){
+    printf("\tWelcome to Narok Library\n");
+    printf("Miss Nancy\n");
+    printf("1:Add user\n");
+    printf("2:Add Book\n");
+    printf("3:Borrow Book\n");
+    printf("4:Return Book\n");
+    printf("5:Delete User\n");
+    printf("6:Delete book\n");
+    printf("7:Logout\n");
+
+    return 0;
+}
